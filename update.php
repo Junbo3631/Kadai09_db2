7f6388493b4769d99f689e1d641cff54b68aeec2
Junbo3631 <junbo.3631@gmail.com>
2025-01-10 03:26:17 +0900

    Update update.php

--- update.php
+++ update.php
@@ -36,7 +36,7 @@ $id    = $_POST['id'];
 try {
     $db_name = 'junbo3631_3size_db';    //データベース名
     $db_id   = 'junbo3631_3size_db';      //アカウント名
-    $db_pw   = 'junko3631';      //パスワード：MAMPは'root'
+    $db_pw   = '';      //パスワード：MAMPは'root'
     $db_host = 'mysql3104.db.sakura.ne.jp'; //DBホスト
     $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
 } catch (PDOException $e) {
@@ -47,7 +47,7 @@ try {
 // $host = 'mysql3104.db.sakura.ne.jp';
 // $dbname = 'junbo3631_3size_db';
 // $username = 'junbo3631_3size_db';
-// $password = 'junko3631';
+// $password = '';
 
 
     // データベース接続
